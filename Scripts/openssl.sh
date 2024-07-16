fe58abaaa52411cc4f66ffa9410db39c639e21e6
Bonguides <bonguides25@gmail.com>
2024-07-16 16:50:02 +0700

    Update openssl.sh

--- Scripts/openssl.sh
+++ Scripts/openssl.sh
@@ -2,9 +2,9 @@
 sudo yum -y update
 sudo yum install -y make gcc perl-core pcre-devel wget zlib-devel
 # wget https://ftp.openssl.org/source/openssl-3.1.1.tar.gz
-# wget https://www.openssl.org/source/openssl-3.1.6.tar.gz
+wget https://www.openssl.org/source/openssl-3.1.6.tar.gz
 
-wget https://src.fedoraproject.org/lookaside/pkgs/openssl/openssl-3.1.1.tar.gz/sha512/8ba9dd6ab87451e126c19cc106ccd1643ca48667d6c37504d0ab98205fbccf855fd0db54474b4113c4c3a15215a4ef77a039fb897a69f71bcab2054b2effd1d9/openssl-3.1.1.tar.gz
+# wget https://src.fedoraproject.org/lookaside/pkgs/openssl/openssl-3.1.1.tar.gz/sha512/8ba9dd6ab87451e126c19cc106ccd1643ca48667d6c37504d0ab98205fbccf855fd0db54474b4113c4c3a15215a4ef77a039fb897a69f71bcab2054b2effd1d9/openssl-3.1.1.tar.gz
 sudo tar -xzvf openssl-3*.tar.gz
 cd openssl-3*/
 ./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
