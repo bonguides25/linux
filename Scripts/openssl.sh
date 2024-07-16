a05e44afc3b62b1c7d0c037efa235b5cb60dd3f1
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-07-16 15:59:22 +0700

    Update openssl.sh

---
+++ Scripts/openssl.sh
@@ -0,0 +1,12 @@
+sudo yum -y update
+sudo yum install -y make gcc perl-core pcre-devel wget zlib-devel
+wget https://ftp.openssl.org/source/openssl-3.1.1.tar.gz
+sudo tar -xzvf openssl-3*.tar.gz
+cd openssl-3*/
+./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
+sudo make -j ${nproc} 
+sudo make test
+sudo make install -j ${nproc} 
+echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64" >> /etc/profile.d/openssl.sh
+source /etc/profile.d/openssl.sh
+openssl version
