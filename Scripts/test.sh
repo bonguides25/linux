78924846aaeb99aa6be000b317ffd7a474d7ae38
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-07-19 18:33:54 +0700

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -7,4 +7,18 @@ sudo ufw allow http
 sudo ufw allow https
 sudo apt install mariadb-server mariadb-client -y
 sudo systemctl enable mariadb && sudo systemctl start mariadb
-sudo apt install php7.4 php7.4-fpm -y
+sudo apt install php php-fpm -y
+
+nginx_version=$(nginx -v 2>&1); echo $version | grep -o '[0-9.]*'
+php_version=php -v | grep ^PHP | cut -d' ' -f2
+db_version=mysql -V | awk '{sub(/,$/,"",$5);print $5}'
+
+
+# Display generated passwords to log file.
+echo "................................................................"
+echo ".....          The installation was successfull            ....."
+echo "................................................................"
+echo "Nginx: " $nginx_version
+echo "PHP: " $php_version
+echo "Database: " $db_version
+echo "................................................................"
