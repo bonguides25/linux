fb7e4c99ae40cbdef6a582d14792c0938211e250
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-07-19 18:41:36 +0700

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -12,6 +12,7 @@ sudo apt install php php-fpm -y
 nginx_version=$(nginx -v 2>&1 | grep -o '[0-9.]*')
 php_version=$(php -v | grep ^PHP | cut -d' ' -f2)
 db_version=$(mysql -V | awk '{sub(/,$/,"",$5);print $5}')
+pub_ipv4=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com)
 
 
 # Generated installation report
@@ -21,4 +22,5 @@ echo "................................................................"
 echo "Nginx: " $nginx_version
 echo "PHP: " $php_version
 echo "Database: " $db_version
+echo "Public IPv4: $pub_ipv4"
 echo "................................................................"
