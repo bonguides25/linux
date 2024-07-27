37d7b0a7abf598a0af9340cd41cc89dd3738e529
Bonguides <bonguides25@gmail.com>
2024-07-27 21:50:16 +0700

    Update wslusbkernel.sh

--- Scripts/wslusbkernel.sh
+++ Scripts/wslusbkernel.sh
@@ -1,1 +1,21 @@
-
+##############################################################################
+# Build WSL2 kernel with usb-storage support
+# menuconfig -> Device Drivers --> USB support ---> USB Mass Storage support
+##############################################################################
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential flex bison libgtk2.0-dev libelf-dev \
+    libncurses-dev autoconf libudev-dev libtool zip unzip v4l-utils \
+    libssl-dev python3-pip cmake git iputils-ping net-tools dwarves bc
+cd /tmp
+sudo git clone https://github.com/microsoft/WSL2-Linux-Kernel.git
+cd WSL2-Linux-Kernel
+sudo cp /proc/config.gz config.gz
+sudo gunzip config.gz
+sudo mv config .config
+sudo make menuconfig
+sudo make -j$(nproc)
+sudo make modules_install -j$(nproc)
+sudo make install -j$(nproc)
+sudo mkdir /mnt/c/sources/
+sudo cp -rf vmlinux /mnt/c/sources/
+##############################################################################
